2ce8663d5d68dfd55e0c7719f527ec10c779dd32
roldleans <roldlearns@gmail.com>
2025-08-21 20:22:56 +0800

    navbar

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            AI-GYB
           </span>
         </a>
 
